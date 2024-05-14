44a0d4808b4507463ac26bf19835995a6d01d4ed
Damien Sawyer <damiensawyer@gmail.com>
2024-05-14 10:04:11 +0100

    restored

--- colourmandelbrot.c
+++ colourmandelbrot.c
@@ -15,8 +15,8 @@ int mandelbrot(double x0, double y0) {
     double x = 0.0;
     double y = 0.0;
     int iteration = 0;
-    while (x * x + y * y <= 4 && iteration < MAX_ITER) {
-        double xtemp = x - y * y + x0;
+    while (x * x + y * y <= 4.0 && iteration < MAX_ITER) {
+        double xtemp = x * x - y * y + x0;
         y = 2 * x * y + y0;
         x = xtemp;
         iteration++;
